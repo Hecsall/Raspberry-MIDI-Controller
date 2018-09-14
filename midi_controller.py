412aba347a424c2c638b8d95e65b3beb47e1c530
Simone <simone.denada@gmail.com>
2018-09-14 09:11:19 +0200

    Fixed a bug for multiple buttons

--- midi_controller.py
+++ midi_controller.py
@@ -92,11 +92,11 @@ def main():
       # footcontroller.btn4.when_released = lambda : footcontroller.sendMIDI(type=CONTROLLER_CHANGE, channel=SUSTAIN, value=0)
       
       # Toggle behaviour (Press=ON, Press again=OFF)
-      footcontroller.btn4.when_pressed  = lambda : footcontroller.sendMIDI(type=CONTROLLER_CHANGE, channel=0x66) # 0x50 = General purpose controller 1
+      footcontroller.btn4.when_pressed  = lambda : footcontroller.sendMIDI(type=CONTROLLER_CHANGE, channel=0x66)
       
-      footcontroller.btn17.when_pressed  = lambda : footcontroller.sendMIDI(type=CONTROLLER_CHANGE, channel=0x67) # 0x51 = General purpose controller 2
+      footcontroller.btn17.when_pressed  = lambda : footcontroller.sendMIDI(type=CONTROLLER_CHANGE, channel=0x67)
       
-      # Possible free MIDI Channels:
+      # Usually free MIDI Channels:
       # 0x50, 0x51, 0x52, 0x53, 0x55, 0X56, 0X57, 0X59, 0X5A, 
       # 0X66, 0X67, 0X68, 0X69, 0X6A, 0X6B, 0X6C, 0X6D, 0X6E, 
       # 0X6F, 0X70, 0X71, 0X72, 0X73, 0X74, 0X75, 0X76, 0X77
