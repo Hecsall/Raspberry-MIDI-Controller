f31e22fb583fd658a8bc76e93b6b108b5861def9
Simone <simone.denada@gmail.com>
2018-10-05 08:51:52 +0200

    still testing bpm

--- other/test_bpm_counter.py
+++ other/test_bpm_counter.py
@@ -5,7 +5,8 @@ import datetime
 print("Started")
 
 
-with mido.open_input('f_midi:f_midi 16:0') as inport:
+# with mido.open_input('f_midi:f_midi 16:0') as inport: # Real port on raspberry
+with mido.open_input('Mido', virtual=True) as inport: # Virtual port for PC testing
 
     # Current start time
     last_time = datetime.datetime.now()
@@ -23,15 +24,9 @@ with mido.open_input('f_midi:f_midi 16:0') as inport:
         diff_seconds = diff.microseconds
 
         # Converto in millisecondi 
-        ms = (diff_seconds / 1000) * 4
+        ms = (diff_seconds / 1000)
         
         bpm = 60000 / ms
         print(bpm)
 
         last_time = new_time
-
-'''
-
-BPM = 1 beat for minute
-
-'''
