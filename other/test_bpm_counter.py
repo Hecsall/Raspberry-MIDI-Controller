4379e833d2c0e58b2f769d7ca363c71375314b7b
Simone <simone.denada@gmail.com>
2018-11-28 16:24:38 +0100

    boh

--- other/test_bpm_counter.py
+++ other/test_bpm_counter.py
@@ -1,32 +1,51 @@
 import mido
 import datetime
+import time
 
 
 print("Started")
 
 
+
 # with mido.open_input('f_midi:f_midi 16:0') as inport: # Real port on raspberry
 with mido.open_input('Mido', virtual=True) as inport: # Virtual port for PC testing
 
-    # Current start time
-    last_time = datetime.datetime.now()
-    new_time = datetime.datetime.now()
+    time.sleep(1)
+    timeout = time.time() + 1   # 1 second from now
 
-    for msg in inport:
+    while True:
+        thistime = time.time()
+        if thistime > timeout:
+            break
+        else:
+            
+            for msg in inport:
+                print(time.time())
+                thistime = time.time()
+                
+
+
+
+# Current start time
+    # last_time = datetime.datetime.now()
+    # new_time = datetime.datetime.now()
+
+
+#     for msg in inport:
         
-        #Tempo di questo tic
-        new_time = datetime.datetime.now()
+#         #Tempo di questo tic
+#         new_time = datetime.datetime.now()
 
-        # Differenza di datetime
-        diff = new_time - last_time
+#         # Differenza di datetime
+#         diff = new_time - last_time
         
-        # Differenza di secondi
-        diff_seconds = diff.microseconds
+#         # Differenza di secondi
+#         diff_seconds = diff.microseconds
 
-        # Converto in millisecondi 
-        ms = (diff_seconds / 1000)
+#         # Converto in millisecondi 
+#         ms = (diff_seconds / 1000)
         
-        bpm = 60000 / ms
-        print(bpm)
+#         bpm = 60000 / ms
+#         print(bpm)
 
-        last_time = new_time
+#         last_time = new_time
